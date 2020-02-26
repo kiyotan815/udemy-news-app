75c7e1d10ce2a76353b70f765aea53e5bbd21dda
kiyotan815 <kyt.ibaragi0815@gmail.com>
2020-02-26 22:26:31 +0900

    newsApi

--- App.jsx
+++ App.jsx
@@ -1,106 +1,50 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {
-  StyleSheet, Text, View, Image,
+  StyleSheet, FlatList, SafeAreaView,
 } from 'react-native';
+import axios from 'axios';
+import ListItem from './components/ListItem';
+import { newsApiKey } from './constants';
+
+const URL = `http://newsapi.org/v2/top-headlines?country=jp&apiKey=${newsApiKey}`;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  itemContainer: {
-    height: 100,
-    width: '100%',
-    borderColor: 'gray',
-    borderWidth: 1,
-    flexDirection: 'row',
-  },
-  leftContainer: {
-    width: 100,
-  },
-  rightContainer: {
-    flex: 1,
-    flexDirection: 'column',
-    padding: 10,
-    justifyContent: 'space-between',
-  },
-  text: {
-    fontSize: 16,
-  },
-  subText: {
-    fontSize: 12,
-    color: 'gray',
   },
 });
 
 export default function App() {
+  const [articles, setArticles] = useState([]);
+
+  const fetchArticles = async () => {
+    try {
+      const response = await axios.get(URL);
+      setArticles(response.data.articles);
+    } catch (error) {
+      setArticles([]);
+    }
+  };
+
+  useEffect(() => {
+    fetchArticles();
+  }, []);
+
+
   return (
-    <View style={styles.container}>
-      <View style={styles.itemContainer}>
-        <View style={styles.leftContainer}>
-          <Image
-            style={{ width: 100, height: 100 }}
-            source={{ uri: 'https://picsum.photos/200/300' }}
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={articles}
+        renderItem={({ item }) => (
+          <ListItem
+            imageUrl={item.urlToImage}
+            title={item.title}
+            author={item.author}
           />
-        </View>
-        <View style={styles.rightContainer}>
-          <Text style={styles.text} numberOfLines={3}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            Ut enim ad minim veniam, quis nostrud exercitation ullamco
-            laboris nisi ut aliquip ex ea commodo consequat.
-            Duis aute irure dolor in reprehenderit in voluptate
-            velit esse cillum dolore eu fugiat nulla pariatur.
-            Excepteur sint occaecat cupidatat non proident,
-            sunt in culpa qui officia deserunt mollit anim id est laborum.
-          </Text>
-          <Text style={styles.subText}>React News</Text>
-        </View>
-      </View>
-      <View style={styles.itemContainer}>
-        <View style={styles.leftContainer}>
-          <Image
-            style={{ width: 100, height: 100 }}
-            source={{ uri: 'https://picsum.photos/200/300' }}
-          />
-        </View>
-        <View style={styles.rightContainer}>
-          <Text style={styles.text} numberOfLines={3}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            Ut enim ad minim veniam, quis nostrud exercitation ullamco
-            laboris nisi ut aliquip ex ea commodo consequat.
-            Duis aute irure dolor in reprehenderit in voluptate
-            velit esse cillum dolore eu fugiat nulla pariatur.
-            Excepteur sint occaecat cupidatat non proident,
-            sunt in culpa qui officia deserunt mollit anim id est laborum.
-          </Text>
-          <Text style={styles.subText}>React News</Text>
-        </View>
-      </View>
-      <View style={styles.itemContainer}>
-        <View style={styles.leftContainer}>
-          <Image
-            style={{ width: 100, height: 100 }}
-            source={{ uri: 'https://picsum.photos/200/300' }}
-          />
-        </View>
-        <View style={styles.rightContainer}>
-          <Text style={styles.text} numberOfLines={3}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            Ut enim ad minim veniam, quis nostrud exercitation ullamco
-            laboris nisi ut aliquip ex ea commodo consequat.
-            Duis aute irure dolor in reprehenderit in voluptate
-            velit esse cillum dolore eu fugiat nulla pariatur.
-            Excepteur sint occaecat cupidatat non proident,
-            sunt in culpa qui officia deserunt mollit anim id est laborum.
-          </Text>
-          <Text style={styles.subText}>React News</Text>
-        </View>
-      </View>
-    </View>
+        )}
+        keyExtractor={(item) => item.title}
+      />
+    </SafeAreaView>
   );
 }
---
+++ components/ListItem.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import {
+  StyleSheet, Text, View, Image,
+} from 'react-native';
+import PropTypes from 'prop-types';
+
+const styles = StyleSheet.create({
+  itemContainer: {
+    height: 100,
+    width: '100%',
+    borderColor: 'gray',
+    borderWidth: 1,
+    flexDirection: 'row',
+  },
+  leftContainer: {
+    width: 100,
+  },
+  rightContainer: {
+    flex: 1,
+    flexDirection: 'column',
+    padding: 10,
+    justifyContent: 'space-between',
+  },
+  text: {
+    fontSize: 16,
+  },
+  subText: {
+    fontSize: 12,
+    color: 'gray',
+  },
+});
+
+const ListItem = ({ author, imageUrl, title }) => (
+  <View style={styles.itemContainer}>
+    <View style={styles.leftContainer}>
+      {!!imageUrl && (
+        <Image
+          style={{ width: 100, height: 100 }}
+          source={{ uri: imageUrl }}
+        />
+      )}
+    </View>
+    <View style={styles.rightContainer}>
+      <Text style={styles.text} numberOfLines={3}>
+        {title}
+      </Text>
+      <Text style={styles.subText}>{author}</Text>
+    </View>
+  </View>
+);
+
+ListItem.propTypes = {
+  author: PropTypes.string,
+  imageUrl: PropTypes.string,
+  title: PropTypes.string,
+};
+
+ListItem.defaultProps = {
+  author: 'React news',
+  imageUrl: 'https://pucsum.photos/200/300',
+  title: 'React',
+};
+
+
+export default ListItem;
